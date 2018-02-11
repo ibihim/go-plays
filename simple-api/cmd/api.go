ce1206a00b0c6108d11c018717650473063fa096
Krzysztof Ostrowski <ibihim@posteo.de>
2018-02-11 09:42:37 +0100

    simple api example

---
+++ simple-api/cmd/api.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	var router = mux.NewRouter()
+
+	router.HandleFunc("/healthcheck", healthcheck).Methods("GET")
+	router.HandleFunc("/message", handleQryMessage).Methods("GET")
+	router.HandleFunc("/m/{msg}", handleUrlMessage).Methods("GET")
+
+	headersOk := handlers.AllowedHeaders([]string{"Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"})
+
+	fmt.Println("Runnin`g Server!")
+	log.Fatal(http.ListenAndServe(":3000", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
+}
+
+func handleQryMessage(w http.ResponseWriter, r *http.Request) {
+	vars := r.URL.Query()
+	message := vars.Get("msg")
+
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
+}
+
+func handleUrlMessage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	message := vars["msg"]
+
+	json.NewEncoder(w).Encode(map[string]string{"message": message})
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Still alive!")
+}
