b971b7f01bc837efc9155c38caf5918a69218a8e
Krzysztof Ostrowski <krzysztof.ostrowski@posteo.de>
2018-05-02 23:02:11 +0200

    */cookie?/*: add cookie plays within golang

--- cmd/cookies/main.go
+++ cmd/cookies/main.go
@@ -1,46 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/ibihim/go-plays/pkg/cookie"
 )
 
-func helloWorld(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "Hello world!")
-}
-
-func redirect(res http.ResponseWriter, req *http.Request) {
-	http.Redirect(res, req, "http://localhost:8080", http.StatusFound)
-}
-
-func setCookie(cookieName, cookieValue string, f http.HandlerFunc) http.HandlerFunc {
-	return func(res http.ResponseWriter, req *http.Request) {
-		http.SetCookie(res, &http.Cookie{
-			Name:     cookieName,
-			Value:    cookieValue,
-			HttpOnly: true,
-		})
-
-		f(res, req)
-	}
-}
-
 func main() {
-	http.HandleFunc("/", setCookie("root", "hier ist 8080?", helloWorld))
-
-	go func() {
-		port := "8081"
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", helloWorld)
-		mux.HandleFunc("/8080", setCookie("8081", "redirect inc", redirect))
-		log.Printf("Listening on :%s\n", port)
-		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), mux))
-	}()
-
-	port := "8080"
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", setCookie("8080", "hello world!", helloWorld))
-	log.Printf("Listening on :%s\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), mux))
+	cookie.NewDiffPathServer()
 }
