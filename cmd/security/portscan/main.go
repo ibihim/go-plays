55c61c3199a6029ff55ab4367877e04b5255f728
Krzysztof Ostrowski <krzysztof.ostrowski@posteo.de>
2018-11-08 07:32:01 +0100

    security:dump of local stuff

--- cmd/security/portscan/main.go
+++ cmd/security/portscan/main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-    "strconv"
-    "log"
-    "net"
-    "time"
+	"log"
+	"net"
+	"strconv"
+	"time"
 )
 
 var ipToScan = "127.0.0.1"
 var minPort = 0
-var maxPort = 1024
+var maxPort = 9999
 
 func main() {
-    activeThreads := 0
-    doneChannel := make(chan bool)
+	activeThreads := 0
+	doneChannel := make(chan bool)
 
-    for port := minPort; port <= maxPort; port++ {
-        go testTcpConnection(ipToScan, port, doneChannel)
-        activeThreads++
-    }
+	for port := minPort; port <= maxPort; port++ {
+		go testTcpConnection(ipToScan, port, doneChannel)
+		activeThreads++
+	}
 
-    // Wait for all threads to finish
-    for activeThreads > 0 {
-        <- doneChannel
-        activeThreads--
-    }
+	// Wait for all threads to finish
+	for activeThreads > 0 {
+		<-doneChannel
+		activeThreads--
+	}
 }
 
 func testTcpConnection(ip string, port int, doneChannel chan bool) {
-    _, err := net.DialTimeout("tcp", ip + ":" + strconv.Itoa(port), time.Second*10)
-    if err == nil {
-        log.Printf("Port %d: Open\n", port)
-    }
-    doneChannel <- true
+	_, err := net.DialTimeout("tcp", ip+":"+strconv.Itoa(port), time.Second*10)
+	if err == nil {
+		log.Printf("Port %d: Open\n", port)
+	}
+	doneChannel <- true
 }
-
