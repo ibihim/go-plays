3c1ddc31118655ba77f7b82fcaa67ce974a8f5f7
Krzysztof Ostrowski <krzysztof.ostrowski@posteo.de>
2019-03-02 23:44:38 +0100

    cli/uniq: playing with unix like uniq

--- cmd/cli/uniq/main.go
+++ cmd/cli/uniq/main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	counts := make(map[string]int)
-	files := os.Args[1:]
+	// params
+	count := flag.Bool("c", false, "prefix lines by the number of occurences")
+	sortOutput := flag.Bool("s", false, "sort output")
+	files := flag.Args()
+	flag.Parse()
 
+	// model
+	counts := make(map[string]int)
+
+	// view
+	output := []string{}
+
+	// fill model
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		mapLines(os.Stdin, counts)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -20,22 +34,57 @@ func main() {
 				continue
 			}
 
-			countLines(f, counts)
+			mapLines(f, counts)
 			f.Close()
 		}
 	}
+
+	// view model
 	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", line)
+		occurence := ""
+
+		if *count {
+			occurence += fmt.Sprintf("%d", n)
 		}
+
+		output = append(output, fmt.Sprintf("%s\t%s", occurence, line))
+	}
+
+	if *sortOutput {
+		parseStartInt := func(str string) int64 {
+			num, err := strconv.ParseInt(
+				strings.Split(str, "\t")[0],
+				10,
+				64,
+			)
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "uniq: %v\n", err)
+				return 0
+			}
+
+			return num
+		}
+
+		sort.Slice(output, func(i, j int) bool {
+			fmt.Println(parseStartInt(output[j]))
+
+			return parseStartInt(output[i]) < parseStartInt(output[j])
+		})
+	}
+
+	for _, line := range output {
+		fmt.Println(line)
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+// mapLines load the input lines as keys and occurence as value
+func mapLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
 	}
-
-	// NOTE: ignoring potential errors from imput.Err()
+	if err := input.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
